1631d076a6cf0cdfb642308b72a7fde1cbebc12b
Ambarish Karole <karoleam@gmail.com>
2014-04-06 18:55:50 -0700

    2nd commit

--- gen/com/flufighter/brace/R.java
+++ gen/com/flufighter/brace/R.java
@@ -37,7 +37,8 @@ public final class R {
          */
         public static final int activity_item_list=0x7f030001;
         public static final int activity_item_twopane=0x7f030002;
-        public static final int fragment_item_detail=0x7f030003;
+        public static final int fragment=0x7f030003;
+        public static final int fragment_item_detail=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
